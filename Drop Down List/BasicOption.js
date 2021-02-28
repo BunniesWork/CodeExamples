06708c6666deb67742bdafff9aa85b003ab4ec9c
Bunnies Work ® <78593074+BunniesWork@users.noreply.github.com>
2021-02-28 10:55:38 +0700

    Create BasicOption.js

---
+++ Drop Down List/BasicOption.js
@@ -0,0 +1,13 @@
+const Dropdown = () => {
+    return (
+        <>
+            <select id = "dropdown">
+            <option value="N/A">N/A</option>
+            <option value="1"><div className="bg-blue-300"></div>1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+            <option value="4">4</option>
+            </select>      
+        </>
+    )
+}
