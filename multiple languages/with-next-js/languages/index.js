dd22232ebb80228334a86c4116474835d348e61a
Bunnies Work ® <78593074+BunniesWork@users.noreply.github.com>
2021-03-08 20:24:00 +0700

    Simbah can't relate 👻

---
+++ multiple languages/with-next-js/languages/index.js
@@ -0,0 +1,13 @@
+import en from './en.json';
+import id from './id.json';
+import kig from './kig.json';
+import jawa from './jawa.json';
+
+export const dictionaryList = { en, id, kig, jawa };
+
+export const languageOptions = {
+  en: 'English',
+  id: 'Bahasa Indonesia',
+  kig: 'Krama Inggil',
+  jawa: 'Boso Jowo (Ngoko)'
+};
